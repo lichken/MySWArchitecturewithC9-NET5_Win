ec5715707e8a4c5e31e57dff45d3f0aaee07654d
Lichken <lichken@gmail.com>
2021-04-10 21:29:39 -0700

    add ch 14

---
+++ ch14/WWTravelClubDB/LibraryDesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace WWTravelClubDB
+{
+    public class LibraryDesignTimeDbContextFactory
+        : IDesignTimeDbContextFactory<MainDBContext>
+    {
+        private const string endpoint = "<your account endpoint>";
+        private const string key = "<your account keys>";
+        private const string databaseName = "packagesdb";
+
+        public MainDBContext CreateDbContext(params string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<MainDBContext>();
+            builder.UseCosmos(endpoint, key, databaseName);
+            return new MainDBContext(builder.Options);
+        }
+    }
+
+}
